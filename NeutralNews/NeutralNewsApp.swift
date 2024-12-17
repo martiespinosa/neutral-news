1f111dc4257bc5799d1f0aaa335a70ad3925a0c6
Martí Espinosa <martiespinosa99@gmail.com>
2024-12-17 15:59:52 +0100

    Initial Commit

---
+++ NeutralNews/NeutralNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeutralNewsApp.swift
+//  NeutralNews
+//
+//  Created by Martí Espinosa Farran on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct NeutralNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
