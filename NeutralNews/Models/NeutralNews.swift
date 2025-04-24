eaaa63642854fea7390feaa2894e9df2f1c975b1
Martí Espinosa <martiespinosa99@gmail.com>
2025-04-24 19:48:23 +0200

    fix: add mock image

--- NeutralNews/Models/NeutralNews.swift
+++ NeutralNews/Models/NeutralNews.swift
@@ -21,7 +21,7 @@ struct NeutralNews: Codable, Identifiable {
         neutralTitle: "Lorem ipsum dolor sit amet",
         neutralDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         category: "Category",
-        imageUrl: nil,
+        imageUrl: "https://www.lavanguardia.com/files/og_thumbnail/files/fp/uploads/2025/04/22/68075b725f598.r_d.1714-2017-0.jpeg",
         createdAt: .now,
         updatedAt: .now,
         group: 0
